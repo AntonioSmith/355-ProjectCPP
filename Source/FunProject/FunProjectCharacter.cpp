60584edd3b50f32cd0cd9c2bf987cbd71332e07b
Antonio <wapasmith@gmail.com>
2021-11-23 15:55:36 -0500

    Class Progress pt. 1

--- Source/FunProject/FunProjectCharacter.cpp
+++ Source/FunProject/FunProjectCharacter.cpp
@@ -67,35 +67,6 @@ void AFunProjectCharacter::SetupPlayerInputComponent(class UInputComponent* Play
 	PlayerInputComponent->BindAxis("TurnRate", this, &AFunProjectCharacter::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 	PlayerInputComponent->BindAxis("LookUpRate", this, &AFunProjectCharacter::LookUpAtRate);
-
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &AFunProjectCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &AFunProjectCharacter::TouchStopped);
-
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AFunProjectCharacter::OnResetVR);
-}
-
-
-void AFunProjectCharacter::OnResetVR()
-{
-	// If FunProject is added to a project via 'Add Feature' in the Unreal Editor the dependency on HeadMountedDisplay in FunProject.Build.cs is not automatically propagated
-	// and a linker error will result.
-	// You will need to either:
-	//		Add "HeadMountedDisplay" to [YourProject].Build.cs PublicDependencyModuleNames in order to build successfully (appropriate if supporting VR).
-	// or:
-	//		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
-
-void AFunProjectCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		Jump();
-}
-
-void AFunProjectCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		StopJumping();
 }
 
 void AFunProjectCharacter::TurnAtRate(float Rate)
